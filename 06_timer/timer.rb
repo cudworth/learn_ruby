1869acc3333666962e69fcb19746a05a18b76ac4
Austin Cudworth <austincudworth@gmail.com>
2020-03-19 20:36:01 -0700

    Solution to Timer problem

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,29 @@
 class Timer
-  #write your code here
+  def initialize
+    @seconds = 0
+  end
+
+  def seconds
+    @seconds
+  end
+
+  def seconds=(val)
+    @seconds = val
+  end
+
+  def time_string
+    h_m_s
+  end
+
+  private
+
+  def h_m_s
+    s = @seconds
+    h = (s / (60 * 60)).to_s.rjust(2, '0')
+    s = s % (60 * 60)
+    m = (s / 60).to_s.rjust(2, '0')
+    s = (s % 60).to_s.rjust(2, '0')
+    p [h, m, s].join(':')
+  end
+
 end
