52d4318eacdeb97569ca791258836883a67e60f8
Austin Cudworth <austincudworth@gmail.com>
2020-03-19 19:35:30 -0700

    Submitting Pig Latin module solution

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,19 @@
 #write your code here
+def translate phrase
+    vowels = 'aeiouy'
+    words = phrase.split(' ')
+    words = words.map do |word|
+        while not vowels.index word[0]
+            word = unshift_push word
+        end
+        if 'u' == word[0] and 'q' == word[-1]
+            word = unshift_push word
+        end
+        word << 'ay'
+    end
+    p words.join ' '
+end
+
+def unshift_push word
+    word = word[1,word.length] << word[0]
+end
