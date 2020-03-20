153570e38c7b5759179dc4c70904c59068f3e02b
Austin Cudworth <austincudworth@gmail.com>
2020-03-19 20:14:39 -0700

    Solution to exercise 05 - Book titles

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,29 @@
 class Book
-# write your code here
+    def initialize 
+        @title = 'none'
+    end
+
+    def title=(value)
+        @title = titleize value
+    end
+
+    def title
+        @title
+    end
+
+    private
+
+    def titleize text
+        little_words = ['and', 'or', 'over', 'the', 'in', 'of', 'a', 'an']
+        text = text.split ' '
+        text = text.map do |word|
+            if (!little_words.index word)
+                word.capitalize
+            else
+                word
+            end
+        end
+        text[0] = text[0].capitalize
+        text.join ' '
+    end
 end
